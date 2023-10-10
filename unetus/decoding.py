c0bd6a16b4141d7779ef097c9947e7fdc3f61db1
beifa <beifabeifa@mail.ru>
2023-10-10 20:42:28 +0300

    add decoder

---
+++ unetus/decoding.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+from .convblock import convBlock
+
+
+class Decoder(nn.Module):
+    def __init__(
+        self,
+        in_chanels: int,
+        num_block: int,
+        pool: str,
+        activation: str,
+        normolization: str
+    ):
+        super().__init__()
+        self.blocks = nn.ModuleList()
+        self.unsample = nn.Upsample(
+            scale_factor=2,
+            align_corners=False,
+            mode='trilinear'
+        )
+        for _ in range(num_block):
+            self.convblock = convBlock(
+                in_chanels=in_chanels*3,
+                out_chanels=in_chanels,
+                pool=pool,
+                activation=activation,
+                normolization=normolization
+            )
+            self.blocks.append(self.convblock)
+            in_chanels //= 2
+
+    def forward(self, x, connection):
+        for encoding_block, skip_connection in zip(self.blocks, connection[::-1]):  # noqa 501
+            x = self.unsample(x)
+            x = torch.concat((skip_connection, x), axis=1)
+            x = encoding_block(x)
+        return x
+
+    @property
+    def out_channels(self):
+        return self.blocks[-1].out_channels
