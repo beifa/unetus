3ab7bc1ba3749ae4638745557a16c2aab2a9362b
beifa <beifabeifa@mail.ru>
2023-10-13 22:19:44 +0300

    add test

--- test/test_unetus.py
+++ test/test_unetus.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import pytest
 from unetus.unetus import Unet3D
 
 
@@ -70,3 +71,86 @@ def test_cuda_Unet3D():
     # [1, 1, 32, 32, 32]))
 
     assert 1 == 1
+
+
+def test_num_block_dim_data():
+    r""" test num block and small dim data"""
+    with pytest.raises(
+        ValueError,
+        match="Depth model not correct reduce size depth, current value: \\d or up size data"   # noqa 501
+    ):
+        x = torch.randn(1, 1, 8, 8, 8)
+        model = Unet3D(residual=True, unsample_type='transpose')
+        _ = model(x)
+
+
+def test_init_chanels():
+    r""" test init chanels """
+    with pytest.raises(
+        AssertionError,
+        match=r"error set chanels and data chanels not equal, get: \d != input_ch: \d"   # noqa 501
+    ):
+        x = torch.randn(1, 3, 8, 8, 8)
+        model = Unet3D(in_chanels=2, residual=True, unsample_type='transpose')
+        _ = model(x)
+
+
+def test_init_chanels_2():
+    r""" test init chanels """
+    with pytest.raises(
+        AssertionError,
+        match=r"error set chanels and data chanels not equal, get: \d != input_ch: \d"   # noqa 501
+    ):
+        x = torch.randn(1, 2, 8, 8, 8)
+        model = Unet3D(in_chanels=1, residual=True, unsample_type='transpose')
+        _ = model(x)
+
+
+def test_num_axis():
+    r""" test all axis """
+    with pytest.raises(
+        AssertionError,
+        match=r"error all axis should be eaul size get x: \d, y: \d, z: \d"
+    ):
+        x = torch.randn(1, 1, 2, 8, 8)
+        model = Unet3D(in_chanels=1, residual=True, unsample_type='transpose')
+        _ = model(x)
+
+
+def test_num_axis_2():
+    r""" test all axis """
+    with pytest.raises(
+        AssertionError,
+        match=r"error all axis should be eaul size get x: \d, y: \d, z: \d"
+    ):
+        x = torch.randn(1, 1, 8, 8, 1)
+        model = Unet3D(in_chanels=1, residual=True, unsample_type='transpose')
+        _ = model(x)
+
+
+def test_transpose():
+    x = torch.randn(1, 1, 16, 16, 16)
+    model1 = Unet3D(unsample_type='conv')
+    out1 = model1(x)
+    model2 = Unet3D(unsample_type='transpose')
+    out2 = model2(x)
+    assert out1.shape == out2.shape
+
+
+def test_dim_data():
+    r""" test dim==5 """
+    with pytest.raises(
+        ValueError,
+        match=r"model data dim should be equal \(b, c, x, y, z\), get: \d"
+    ):
+        x = torch.randn(1, 16, 16, 16)
+        model = Unet3D()
+        _ = model(x)
+
+
+def test_crop_Unet3D():
+    r""" test case """
+    x = torch.randn(1, 1, 64, 64, 64)
+    model = Unet3D(crop_conn=True)
+    out = model(x)
+    assert all(torch.tensor(out.shape) == torch.tensor([1, 1, 64, 64, 64]))
